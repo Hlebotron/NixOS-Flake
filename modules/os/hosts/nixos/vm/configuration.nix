475f9a692f3ef53c08c99cadeb5cd638997937f3
Hlebotron <stabasov@gmail.com>
2026-04-04 14:06:56 +0200

    Actually change hostname for nixos host vm

--- modules/os/hosts/nixos/vm/configuration.nix
+++ modules/os/hosts/nixos/vm/configuration.nix
@@ -69,7 +69,7 @@
     # };
 
     networking = {
-      hostName = "nixos-probook"; # Define your hostname.
+      hostName = "vm"; # Define your hostname.
       # Pick only one of the below networking options.
       # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
       # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
