b292986a3290895e1578417bd2aeacde31e54039
Hlebotron <stabasov@gmail.com>
2026-04-11 14:59:54 +0200

    Add disko and boot.nix for nixos hosts

--- flake.nix
+++ flake.nix
@@ -12,7 +12,11 @@
     import-tree.url = "github:vic/import-tree";
     wrapper-modules.url = "github:BirdeeHub/nix-wrapper-modules";
 
-    
+
+    disko = {
+      url = "github:nix-community/disko/latest";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     noctalia = {
       url = "github:noctalia-dev/noctalia-shell";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -20,85 +24,13 @@
     nixgl.url = "github:nix-community/nixgl";
     nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-25.11";
     stylix = {
-        url = "github:danth/stylix/release-25.11";
-        inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:danth/stylix/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
   outputs = inputs:
-    inputs.flake-parts.lib.mkFlake {
-      inherit inputs;
-    }
+    inputs.flake-parts.lib.mkFlake
+      { inherit inputs; }
       (inputs.import-tree ./modules);
-  # outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, nixvim, nix-darwin, stylix, ... }@inputs:
-  #   let
-  #     system = "x86_64-linux";
-  #     nixpkgs-overlay = final: prev: {
-  #       unstable = nixpkgs-unstable.legacyPackages.${prev.system};
-  #       # use this variant if unfree packages are needed:
-  #       #unstable = import nixpkgs-unstable {
-  #       #  inherit system;
-  #       #  config.allowUnfree = true;
-  #       #};
-  #     };
-  #     home-manager-overlay = final: prev: {};
-  #   in {
-  #     nixosConfigurations = {
-  #       nixos-probook = nixpkgs.lib.nixosSystem {
-  #         system = "x86_64-linux";
-  #         specialArgs = { inherit inputs; };
-  #         modules = [
-  #           # Overlays-module makes "pkgs.unstable" available in configuration.nix
-  #           ({ config, pkgs, inputs, ... }: { nixpkgs.overlays = [ nixpkgs-overlay ]; })
-  #           ./hosts/nixos-probook/configuration.nix
-  #           #home-manager.nixosModules.home-manager {
-  #           #  home-manager = {
-  #           #    useGlobalPkgs = true; 
-  #           #    useUserPackages = true;
-  #           #    extraSpecialArgs = { inherit inputs; };
-  #           #    users.sasha = import ./home-manager/home.nix;
-  #           #    sharedModules = [
-  #           #        nixvim.homeManagerModules.nixvim
-  #           #        #stylix.homeManagerModules.stylix
-  #           #    ];
-  #           #  };
-  #           #}
-  #           stylix.nixosModules.stylix
-  #         ];
-  #       };
-  #       maxim-dell = nixpkgs.lib.nixosSystem {
-  #         system = "x86_64-linux";
-  #         specialArgs = { inherit inputs; };
-  #         modules = [
-  #           # Overlays-module makes "pkgs.unstable" available in configuration.nix
-  #           ({ config, pkgs, inputs, ... }: { nixpkgs.overlays = [ nixpkgs-overlay ]; })
-  #           ./hosts/maxim-dell/configuration.nix
-  #           stylix.nixosModules.stylix
-  #         ];
-  #       };
-  #       nixos-pc = nixpkgs.lib.nixosSystem {
-  #         system = "x86_64-linux";
-  #         specialArgs = { inherit inputs; };
-  #         modules = [
-  #           # Overlays-module makes "pkgs.unstable" available in configuration.nix
-  #           ({ config, pkgs, inputs, ... }: { nixpkgs.overlays = [ nixpkgs-overlay ]; })
-  #           ./hosts/nixos-pc/configuration.nix
-  #           stylix.nixosModules.stylix
-  #         ];
-  #       };
-  #     };
-  #     darwinConfiguration.MacBook = nix-darwin.lib.darwinSystem {
-  #       system = "aarch64-darwin";
-  #       modules = [ 
-  #           ./hosts/darwin/darwin-configuration.nix 
-  #           #home-manager.darwinModules.home-manager {
-  #           #    home-manager {
-  #           #        users.sasha = import ./home-manager/home.nix;
-  #           #    };
-  #           #    users.users.sasha.home = "/Users/sasha";
-  #           #}
-  #       ];
-  #       specialArgs = { inherit inputs; };
-  #     };
-  #   };
-  }
+}
---
+++ modules/os/features/boot.nix
@@ -0,0 +1,48 @@
+{ self, inputs, ...}:
+
+{
+  flake.modules.nixos = {
+    
+    boot-mbr = { config, lib, pkgs, modulesPath, ... }: {
+      imports = [
+        (modulesPath + "/installer/scan/not-detected.nix")
+      ];
+      boot = {
+        initrd = {
+          availableKernelModules = [ "ahci" "xhci_pci" "virtio_pci" "sr_mod" "virtio_blk" ];
+          kernelModules = [];
+        };
+        
+        kernelModules = [ "kvm-intel" ];
+        extraModulePackages = [];
+      };
+      
+      fileSystems = {
+        "/" = {
+          device = "/dev/disk/by-label/NIXROOT";
+          fsType = "ext4";
+        };
+      };
+      
+      swapDevices = [
+        {
+          device = "/dev/disk/by-label/NIXSWAP";
+        }
+      ];
+
+      nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+    };
+    
+    boot-gpt = { config, lib, pkgs, modulesPath, ... }: {
+      imports = [
+        self.modules.nixos.boot-mbr
+      ];
+      
+      fileSystems."/boot" = {
+        device = "/dev/disk/by-label/NIXBOOT";
+        fsType = "vfat";
+        options = [ "fmask=0022" "dmask=0022" ];
+      };
+    };
+  };
+}
