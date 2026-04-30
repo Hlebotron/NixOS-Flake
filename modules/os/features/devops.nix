8c2473d2e5ed953d60c42f283a5e7b9fe19c0fe9
Hlebotron <stabasov@gmail.com>
2026-04-30 12:40:52 +0200

    Add manifests and test image for K3s

--- modules/os/features/devops.nix
+++ modules/os/features/devops.nix
@@ -10,6 +10,19 @@
       k3s = {
         enable = true;
         role = "server";
+        manifests = {
+          deployments.source = ./Kubernetes/deployment.yaml;
+          services.source = ./Kubernetes/service.yaml;
+          # routing.source = ./Kubernetes/routing.yaml;          
+        };
+        images = [
+          (pkgs.dockerTools.pullImage {
+            imageName = "ghcr.io/immich-app/immich-server";
+            imageDigest = "sha256:c15bff75068effb03f4355997d03dc7e0fc58720c2b54ad6f7f10d1bc57efaa5";
+            hash = inputs.pkgs.lib.fakeHash;
+            finalImageTag = "release";
+          })
+        ];
       };
     };
   };
